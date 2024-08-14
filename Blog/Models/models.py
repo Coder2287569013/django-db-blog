bb1511dcc29d586f7fd38286421ecb0555bfe6d6
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-14 13:37:23 -0230

    Add models

--- Blog/Models/models.py
+++ Blog/Models/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}, {self.date}"
+    
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.auth} on {self.blog}"
