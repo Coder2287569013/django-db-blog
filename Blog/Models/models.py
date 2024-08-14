3557fe758c24dbff433946c29e7fd5b84d5df720
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-08-14 14:00:14 -0230

    Update models.py

--- Blog/Models/models.py
+++ Blog/Models/models.py
@@ -18,4 +18,4 @@ class Comment(models.Model):
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.auth} on {self.blog}"
+        return f"Comment by {self.author}, {self.date}"
